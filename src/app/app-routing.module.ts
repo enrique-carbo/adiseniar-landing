5ef67d4e6c3dc64039a30dab10da47b12962c5f2
ecarbo <enriquecarbo@tutanota.com>
2023-01-18 17:28:50 -0300

    Add pages and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DisenoComponent } from './pages/diseno/diseno.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { WebComponent } from './pages/web/web.component';
+
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, title: 'ADiseniar - Inicio' },
   { path: 'diseno', component: DisenoComponent, title: 'ADiseniar - Logos' },
+  { path: 'web', component: WebComponent, title: 'ADiseniar - Web' },
+  { path: 'portfolio', component: PortfolioComponent, title: 'ADiseniar - Portfolio' },
   { path: '',   redirectTo: '/home', pathMatch: 'full' }, 
   //{ path: '**', component: Error404Component, title: 'Error 404' }, 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
